ceda81d27fe3481d2b6858680e4d7403f83a3294
unknown <belenizetta@gmail.com>
2022-01-27 09:24:33 -0300

    probar

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -80,15 +80,17 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 // @route PUT /api/users
 // @access Private/Admin
 
-export const getUsers = asyncHandler(async (req, res) => {
+export const getUsers = asyncHandler(async (req,res) => {
+    req.user;
     const user = await User.find({ });
     console.log(user);
         res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
+            user
         });
+
+    
+        
+        
 })
 
 // @desc Get user by ID
@@ -113,6 +115,53 @@ export const getUserById = asyncHandler(async (req, res) => {
     }
 
 })
+
+//@desc     Update user profile
+//@route    PUT /api/users/profile
+//@access   Private
+export const updateUserProfile = asyncHandler(async (req, res) => {
+    
+    const { _id } = req.user;    
+
+    const user = await User.findById({_id});
+     
+    if (user){
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = req.body.isAdmin || user.isAdmin;
+        user.password = req.body.password;
+        const updateUserProfile = await user.save();
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error('User not found');
+    }
+});
+
+// @desc    Delete user 
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+export const deleteUser = asyncHandler(async (req,res)=>{
+    //usar findById
+
+    const user = await User.findById(req.params.id);
+ 
+      if (!user) {
+            res.status(404);
+            throw new Error('User not found');          
+        }
+  
+      user.remove();
+      res.status(200).json({name: user.name,email: user.email});
+      throw new Error('User removed');
+});
 
 
 
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
-import { registerUser, authUser, getUserProfile ,getUsers, getUserById } from '../controllers/userController.js';
-import { protect } from '../middlewares/authMiddlewares.js';
+import { registerUser, authUser, getUserProfile ,getUsers, getUserById, deleteUser, updateUserProfile } from '../controllers/userController.js';
+import { protect, admin } from '../middlewares/authMiddlewares.js';
 
 const router = Router();
 
 router.post('/', registerUser);
 router.post('/login', authUser);
 router.get('/profile', protect, getUserProfile);
-router.get('/',protect,getUsers);
-router.get('/:id', protect, getUserById);
+router.get('/',protect,admin,getUsers);
+router.get('/:id', protect, admin, getUserById);
+router.delete('/:id',protect,admin,deleteUser);
+router.put('/profile', protect, updateUserProfile);
 
 export default router;
